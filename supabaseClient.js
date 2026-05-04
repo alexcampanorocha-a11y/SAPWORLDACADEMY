d5208e3283f71084af679e01da9771bb449315db
alexcampanorocha-a11y <alexcampanorocha@gmail.com>
2026-05-04 16:08:58 -0500

    Delete supabaseClient.js

--- supabaseClient.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-// Reemplaza con tus datos reales
-const supabaseUrl = 'https://fmgxthdlvjkufolfqlwj.supabase.co/rest/v1/'; 
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZtZ3h0aGRsdmprdWZvbGZxbHdqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc5MTk4MDQsImV4cCI6MjA5MzQ5NTgwNH0.-2uamfGb8AFTyvXxRquVFVLXtnf-H53-R3vCYc8iq94';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
